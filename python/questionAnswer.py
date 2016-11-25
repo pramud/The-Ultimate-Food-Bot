73893f69f1161d31af03c866b2e3e8feff2cd829
Pramud Bommakanti <naga92@gmail.com>
2016-11-25 09:51:39 +0530

    Correcting is Phone method

--- python/questionAnswer.py
+++ python/questionAnswer.py
@@ -2,14 +2,15 @@ import re
 def isQuestionForPhNo(str):
 	if "ph number" in str:
 		return re.search("ph number (.*)",str).groups()[0].split()[1]
-	elif "phone number":
+	elif "phone number" in str:
 		return re.search("phone number (.*)",str).groups()[0].split()[1]
-	elif "ph no":
+	elif "ph no" in str:
 		return re.search("ph no (.*)",str).groups()[0].split()[1]
 	else:
 		return False
 def answerQuestion(str):
-	if(isQuestionForPhNo(str)):
-		return  isQuestionForPhNo(str)
+	op = isQuestionForPhNo(str)
+	if(op):
+		return  op
 	else:
 		return "UNKNOWN QUESTION"
