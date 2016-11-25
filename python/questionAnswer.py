b41249decd268ac393cff0e2d94d589688dff898
Pramud Bommakanti <naga92@gmail.com>
2016-11-25 09:56:14 +0530

    Getting all the strings after the match

--- python/questionAnswer.py
+++ python/questionAnswer.py
@@ -1,11 +1,11 @@
 import re
 def isQuestionForPhNo(str):
 	if "ph number" in str:
-		return re.search("ph number (.*)",str).groups()[0].split()[1]
+		return re.search("ph number (.*)",str).groups()[0].split()[1:]
 	elif "phone number" in str:
-		return re.search("phone number (.*)",str).groups()[0].split()[1]
+		return re.search("phone number (.*)",str).groups()[0].split()[1:]
 	elif "ph no" in str:
-		return re.search("ph no (.*)",str).groups()[0].split()[1]
+		return re.search("ph no (.*)",str).groups()[0].split()[1:]
 	else:
 		return False
 def answerQuestion(str):
